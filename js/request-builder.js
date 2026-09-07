9eed41c3068fb4002d366c66072e2f347fccf68e
bartolkaruza <bartol.k@gmail.com>
2026-09-07 14:22:23 +0000

    change to chassi_number_info to match the newer spec

--- js/request-builder.js
+++ js/request-builder.js
@@ -538,7 +538,7 @@
                     issuing_authority_unicode: false,
                     un_distinguishing_sign: false,
                     date_of_registration: false,
-                    chassis_number: false,
+                    chassis_number_info: false,
                 };
                 break;
             case "mvc_full":
