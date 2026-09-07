190cabe20b741c8b33f8420081642556fd56ae1b
bartolkaruza <bartol.k@gmail.com>
2026-09-07 15:27:45 +0000

    Remove the chassis_number_info because there is some spec ambiguity still

--- js/request-builder.js
+++ js/request-builder.js
@@ -538,7 +538,6 @@
                     issuing_authority_unicode: false,
                     un_distinguishing_sign: false,
                     date_of_registration: false,
-                    chassis_number_info: false,
                 };
                 break;
             case "mvc_full":
